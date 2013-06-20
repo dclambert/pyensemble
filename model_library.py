5578e82b9a96626be8004d1f0b5ce10d2bcc138c
dclambert <dcl@panix.com>
2013-06-20 09:35:30 -0500

    factored out model building into its own module added -d option to test_ensemble to load user svm files

---
+++ model_library.py
@@ -0,0 +1,174 @@
+# -*- coding: utf8
+# Author: David C. Lambert [dcl -at- panix -dot- com]
+# Copyright(c) 2013
+# License: Simple BSD
+"""Utility module for building model library"""
+
+from __future__ import print_function
+
+import numpy as np
+
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cluster import KMeans
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.grid_search import IterGrid
+from sklearn.utils import check_random_state
+
+
+# generic model builder
+def build_models(model_class, param_grid):
+    print('Building %s models' % str(model_class).split('.')[-1][:-2])
+
+    models = []
+    for params in IterGrid(param_grid):
+        models.append(model_class(**params))
+
+    return models
+
+
+def build_randomForestClassifiers(random_state=None):
+    param_grid = {
+        'n_estimators': [20, 50, 100],
+        'criterion':  ['gini', 'entropy'],
+        'max_features': [None, 'auto', 'sqrt', 'log2'],
+        'max_depth': [1, 2, 5, 10],
+        'min_density': [0.25, 0.5, 0.75, 1.0],
+        'random_state': [random_state],
+    }
+
+    return build_models(RandomForestClassifier, param_grid)
+
+
+def build_gradientBoostingClassifiers(random_state=None):
+    param_grid = {
+        'max_depth': [1, 2, 5, 10],
+        'n_estimators': [10, 20, 50, 100],
+        'subsample': np.linspace(0.2, 1.0, 5),
+        'max_features': np.linspace(0.2, 1.0, 5),
+    }
+
+    return build_models(GradientBoostingClassifier, param_grid)
+
+
+def build_sgdClassifiers(random_state=None):
+    param_grid = {
+        'loss': ['log', 'modified_huber'],
+        'penalty': ['elasticnet'],
+        'alpha': [0.0001, 0.001, 0.01, 0.1],
+        'learning_rate': ['constant', 'optimal'],
+        'n_iter': [2, 5, 10],
+        'eta0': [0.001, 0.01, 0.1],
+        'l1_ratio': np.linspace(0.0, 1.0, 3),
+    }
+
+    return build_models(SGDClassifier, param_grid)
+
+
+def build_decisionTreeClassifiers(random_state=None):
+    rs = check_random_state(random_state)
+
+    param_grid = {
+        'criterion': ['gini', 'entropy'],
+        'max_features': [None, 'auto', 'sqrt', 'log2'],
+        'max_depth': [None, 1, 2, 5, 10],
+        'min_samples_split': [1, 2, 5, 10],
+        'random_state': [rs.random_integers(100000) for i in xrange(3)],
+    }
+
+    return build_models(DecisionTreeClassifier, param_grid)
+
+
+def build_extraTreesClassifiers(random_state=None):
+    param_grid = {
+        'criterion': ['gini', 'entropy'],
+        'n_estimators': [5, 10, 20],
+        'max_features': [None, 'auto', 'sqrt', 'log2'],
+        'max_depth': [None, 1, 2, 5, 10],
+        'min_samples_split': [2, 5, 10],
+        'random_state': [random_state],
+    }
+
+    return build_models(ExtraTreesClassifier, param_grid)
+
+
+def build_svcs(random_state=None):
+    print('Building SVM models')
+
+    Cs = np.logspace(-7, 2, 10)
+    gammas = np.logspace(-6, 2, 9, base=2)
+    coef0s = [-1.0, 0.0, 1.0]
+
+    models = []
+
+    for C in Cs:
+        models.append(SVC(kernel='linear', C=C, probability=True,
+                          cache_size=1000))
+
+    for C in Cs:
+        for coef0 in coef0s:
+            models.append(SVC(kernel='sigmoid', C=C, coef0=coef0,
+                              probability=True, cache_size=1000))
+
+    for C in Cs:
+        for gamma in gammas:
+            models.append(SVC(kernel='rbf', C=C, gamma=gamma,
+                              cache_size=1000, probability=True))
+
+    param_grid = {
+        'kernel': ['poly'],
+        'C': Cs,
+        'gamma': gammas,
+        'degree': [2],
+        'coef0': coef0s,
+        'probability': [True],
+        'cache_size': [1000],
+    }
+
+    for params in IterGrid(param_grid):
+        models.append(SVC(**params))
+
+    return models
+
+
+def build_kmeansPipelines(random_state=None):
+    print('Building KMeans-Logistic Regression Pipelines')
+
+    param_grid = {
+        'n_clusters': xrange(5, 205, 5),
+        'init': ['k-means++', 'random'],
+        'n_init': [1, 2, 5, 10],
+        'random_state': [random_state],
+    }
+
+    models = []
+
+    for params in IterGrid(param_grid):
+        km = KMeans(**params)
+        lr = LogisticRegression()
+        models.append(Pipeline([('km', km), ('lr', lr)]))
+
+    return models
+
+
+models_dict = {
+    'svc': build_svcs,
+    'sgd': build_sgdClassifiers,
+    'gbc': build_gradientBoostingClassifiers,
+    'dtree': build_decisionTreeClassifiers,
+    'forest': build_randomForestClassifiers,
+    'extra': build_extraTreesClassifiers,
+    'kmp': build_kmeansPipelines,
+}
+
+
+def build_model_library(model_types=['dtree'], random_seed=None):
+    models = []
+    for m in model_types:
+        models.extend(models_dict[m](random_state=random_seed))
+    return models
--- test_ensemble.py
+++ test_ensemble.py
@@ -23,12 +23,11 @@ The user can choose from the following candidate models:
     extra   : Extra Trees
     kmp     : KMeans->LogisticRegression Pipelines
 
-usage: test_ensemble.py [-h] -D DB_NAME
-                        [-M {svc,sgd,gbc,dtree,forest,extra,kmp}
-                                   [{svc,sgd,gbc,dtree,forest,extra,kmp} ...]]
-                        [-S {accuracy,rmse,xentropy,f1}] [-b N_BAGS]
+usage: test_ensemble.py [-h] -D DB_NAME -d TRAIN_FILE
+                        [-M {svc,sgd,gbc,dtree,forest,extra,kmp} [{svc,sgd,gbc,dtree,forest,extra,kmp} ...]]
+                        [-S {f1,auc,rmse,accuracy,xentropy}] [-b N_BAGS]
                         [-f BAG_FRACTION] [-B N_BEST] [-m MAX_MODELS]
-                        [-F N_FOLDS] [-p PRUNE_FRACTION] [-e EPSILON]
+                        [-F N_FOLDS] [-p PRUNE_FRACTION] [-u] [-e EPSILON]
                         [-t TEST_SIZE] [-s SEED] [-v]
 
 Test EnsembleClassifier
@@ -36,11 +35,11 @@ Test EnsembleClassifier
 optional arguments:
   -h, --help            show this help message and exit
   -D DB_NAME            file for backing store
-  -M {svc,sgd,gbc,dtree,forest,extra,kmp}
-                                    [{svc,sgd,gbc,dtree,forest,extra,kmp} ...]
+  -d TRAIN_FILE         testing data in svm format
+  -M {svc,sgd,gbc,dtree,forest,extra,kmp} [{svc,sgd,gbc,dtree,forest,extra,kmp} ...]
                         model types to include as ensemble candidates
                         (default: ['dtree'])
-  -S {accuracy,rmse,xentropy,f1}
+  -S {f1,auc,rmse,accuracy,xentropy}
                         scoring metric used for hillclimbing (default:
                         accuracy)
   -b N_BAGS             bags to create (default: 20)
@@ -52,6 +51,7 @@ optional arguments:
   -F N_FOLDS            internal cross-validation folds (default: 3)
   -p PRUNE_FRACTION     fraction of worst models pruned pre-selection
                         (default: 0.75)
+  -u                    use epsilon to stop adding models (default: False)
   -e EPSILON            score improvement threshold to include new model
                         (default: 0.0001)
   -t TEST_SIZE          fraction of data to use for testing (default: 0.95)
@@ -67,176 +67,13 @@ from argparse import ArgumentParser
 from sklearn.utils import check_random_state
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
-from sklearn.datasets import fetch_mldata
+from sklearn.datasets import fetch_mldata, load_svmlight_file
 from sklearn.grid_search import IterGrid
 from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import train_test_split
 
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.cluster import KMeans
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import SGDClassifier
-from sklearn.linear_model import LogisticRegression
-
 from ensemble import EnsembleSelectionClassifier
-
-
-# generic model builder
-def build_models(model_class, param_grid):
-    print('Building %s models' % str(model_class).split('.')[-1][:-2])
-
-    models = []
-    for params in IterGrid(param_grid):
-        models.append(model_class(**params))
-
-    return models
-
-
-def build_randomForestClassifiers(random_state=None):
-    param_grid = {
-        'n_estimators': [20, 50, 100],
-        'criterion':  ['gini', 'entropy'],
-        'max_features': [None, 'auto', 'sqrt', 'log2'],
-        'max_depth': [1, 2, 5, 10],
-        'min_density': [0.25, 0.5, 0.75, 1.0],
-        'random_state': [random_state],
-    }
-
-    return build_models(RandomForestClassifier, param_grid)
-
-
-def build_gradientBoostingClassifiers(random_state=None):
-    param_grid = {
-        'max_depth': [1, 2, 5, 10],
-        'n_estimators': [10, 20, 50, 100],
-        'subsample': np.linspace(0.2, 1.0, 5),
-        'max_features': np.linspace(0.2, 1.0, 5),
-    }
-
-    return build_models(GradientBoostingClassifier, param_grid)
-
-
-def build_sgdClassifiers(random_state=None):
-    param_grid = {
-        'loss': ['log', 'modified_huber'],
-        'penalty': ['elasticnet'],
-        'alpha': [0.0001, 0.001, 0.01, 0.1],
-        'learning_rate': ['constant', 'optimal'],
-        'n_iter': [2, 5, 10],
-        'eta0': [0.001, 0.01, 0.1],
-        'l1_ratio': np.linspace(0.0, 1.0, 3),
-    }
-
-    return build_models(SGDClassifier, param_grid)
-
-
-def build_decisionTreeClassifiers(random_state=None):
-    rs = check_random_state(random_state)
-
-    param_grid = {
-        'criterion': ['gini', 'entropy'],
-        'max_features': [None, 'auto', 'sqrt', 'log2'],
-        'max_depth': [None, 1, 2, 5, 10],
-        'min_samples_split': [1, 2, 5, 10],
-        'random_state': [rs.random_integers(100000) for i in xrange(3)],
-    }
-
-    return build_models(DecisionTreeClassifier, param_grid)
-
-
-def build_extraTreesClassifiers(random_state=None):
-    param_grid = {
-        'criterion': ['gini', 'entropy'],
-        'n_estimators': [5, 10, 20],
-        'max_features': [None, 'auto', 'sqrt', 'log2'],
-        'max_depth': [None, 1, 2, 5, 10],
-        'min_samples_split': [2, 5, 10],
-        'random_state': [random_state],
-    }
-
-    return build_models(ExtraTreesClassifier, param_grid)
-
-
-def build_svcs(random_state=None):
-    print('Building SVM models')
-
-    Cs = np.logspace(-7, 2, 10)
-    gammas = np.logspace(-6, 2, 9, base=2)
-    coef0s = [-1.0, 0.0, 1.0]
-
-    models = []
-
-    for C in Cs:
-        models.append(SVC(kernel='linear', C=C, probability=True,
-                          cache_size=1000))
-
-    for C in Cs:
-        for coef0 in coef0s:
-            models.append(SVC(kernel='sigmoid', C=C, coef0=coef0,
-                              probability=True, cache_size=1000))
-
-    for C in Cs:
-        for gamma in gammas:
-            models.append(SVC(kernel='rbf', C=C, gamma=gamma,
-                              cache_size=1000, probability=True))
-
-    param_grid = {
-        'kernel': ['poly'],
-        'C': Cs,
-        'gamma': gammas,
-        'degree': [2],
-        'coef0': coef0s,
-        'probability': [True],
-        'cache_size': [1000],
-    }
-
-    for params in IterGrid(param_grid):
-        models.append(SVC(**params))
-
-    return models
-
-
-def build_kmeansPipelines(random_state=None):
-    print('Building KMeans-Logistic Regression Pipelines')
-
-    param_grid = {
-        'n_clusters': xrange(5, 205, 5),
-        'init': ['k-means++', 'random'],
-        'n_init': [1, 2, 5, 10],
-        'random_state': [random_state],
-    }
-
-    models = []
-
-    for params in IterGrid(param_grid):
-        km = KMeans(**params)
-        lr = LogisticRegression()
-        models.append(Pipeline([('km', km), ('lr', lr)]))
-
-    return models
-
-
-def load_data(test_size=0.25, random_state=None):
-    print('\nloading letter data')
-
-    letter = fetch_mldata('letter')
-    X = letter.data
-    y = np.array(letter.target > 12, dtype=int)
-
-    splits = train_test_split(X, y, test_size=test_size,
-                              random_state=random_state)
-    X_train, X_test, y_train, y_test = splits
-
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    return X_train, X_test, y_train, y_test
-
+from model_library import build_model_library
 
 def parse_args():
     parser = ArgumentParser(description='Test EnsembleClassifier')
@@ -245,6 +82,9 @@ def parse_args():
 
     parser.add_argument('-D', dest='db_name', required=True,
                         help='file for backing store')
+
+    parser.add_argument('-d', dest='train_file', required=True,
+                        help='testing data in svm format')
 
     model_choices = ['svc', 'sgd', 'gbc', 'dtree', 'forest', 'extra', 'kmp']
     help_fmt = 'model types to include as ensemble candidates %s' % dflt_fmt
@@ -305,26 +145,21 @@ def parse_args():
 if (__name__ == '__main__'):
     res = parse_args()
 
-    data = load_data(res.test_size, random_state=res.seed)
-    X_train, X_test, y_train, y_test = data
+    X, y = load_svmlight_file(res.train_file)
+    X = X.toarray()
+
+    splits = train_test_split(X, y, test_size=res.test_size,
+                              random_state=res.seed)
+    X_train, X_test, y_train, y_test = splits
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
 
     print('Train/hillclimbing set size: %d' % len(X_train))
     print('              Test set size: %d\n' % len(X_test))
 
-    models_dict = {
-        'svc': build_svcs,
-        'sgd': build_sgdClassifiers,
-        'gbc': build_gradientBoostingClassifiers,
-        'dtree': build_decisionTreeClassifiers,
-        'forest': build_randomForestClassifiers,
-        'extra': build_extraTreesClassifiers,
-        'kmp': build_kmeansPipelines,
-    }
-
-    models = []
-    for m in res.model_types:
-        models.extend(models_dict[m](random_state=res.seed))
-
+    models = build_model_library(res.model_types, res.seed)
     print('built %d models\n' % len(models))
 
     param_dict = {
